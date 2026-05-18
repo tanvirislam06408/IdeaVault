b23793ccb706ac5c84f57cc7e8f147e381207eff
tanvir <mstanvirislam05@gmail.com>
2026-05-18 20:22:25 +0600

    fix card layout

--- src/components/shared/FeaturedCard.jsx
+++ src/components/shared/FeaturedCard.jsx
@@ -1,6 +1,9 @@
 import { CircleDollar } from "@gravity-ui/icons";
-import { Button, Card, Link } from "@heroui/react";
+import { Avatar, Button, Card, Link } from "@heroui/react";
 import Image from "next/image";
+import { AiOutlineLike } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+import { FcLike } from "react-icons/fc";
 const FeaturedCard = ({ idea }) => {
   return (
     <div className="">
@@ -16,7 +19,20 @@ const FeaturedCard = ({ idea }) => {
             </Card.Description>
           </Card.Header>
         </div>
-        <Card.Footer>
+        <Card.Footer className="flex flex-col space-y-4">
+          <div className="flex w-full px-3 justify-between ">
+            <div className="flex items-center gap-2.5">
+              <Avatar size="sm">
+                <Avatar.Image alt="John Doe" src={idea?.author?.photo} />
+                <Avatar.Fallback>JD</Avatar.Fallback>
+              </Avatar>
+              <p>{idea?.author?.name}</p>
+            </div>
+            <div className="flex items-center gap-2.5">
+              <AiOutlineLike />
+              <FaRegComment />
+            </div>
+          </div>
           <Button variant="outline" className={'w-full mt-auto'}>View Details</Button>
         </Card.Footer>
       </Card>
