c14b1f9e37b54b4ceb5abfe310fe7a58c4868cb1
tanvir <mstanvirislam05@gmail.com>
2026-05-22 02:49:43 +0600

    add toggle icon

--- src/components/shared/ThemeToggle.jsx
+++ src/components/shared/ThemeToggle.jsx
@@ -3,13 +3,14 @@ import { Button } from '@heroui/react';
 import { Moon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import React from 'react';
+import { FiSun } from "react-icons/fi";
 
 
 const ThemeToggle = () => {
  const { theme, setTheme } = useTheme();
     return (
         <Button onClick={()=>setTheme(theme === "dark" ? "light" : "dark")} variant='outline' className="text-gray-700 border-none">
-            <Moon size={18} />
+            {theme === "dark" ? <Moon size={18} /> : <FiSun />}
           </Button>
     );
 };
