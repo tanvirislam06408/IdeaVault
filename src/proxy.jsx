07876c1455a8d5ce5e11816fd99559da64f7bd7b
tanvir <mstanvirislam05@gmail.com>
2026-05-22 14:48:37 +0600

    fixed proxy issue

--- src/proxy.jsx
+++ src/proxy.jsx
@@ -20,5 +20,5 @@ export async function proxy(request) {
 // export default function proxy(request) { ... }
  
 export const config = {
-  matcher: ['/add-ideas','/my-ideas','/profile','/interactions','/ideas/:path*'],
+  matcher: ['/add-ideas','/my-ideas','/profile','/interactions','/ideas/:id'],
 }
